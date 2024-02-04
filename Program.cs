2ae37add29199168087e78eea1bb1d753cb738bd
MatiMa <mati.mannisalu@tktk.ee>
2024-02-04 20:44:22 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string UserName = Console.ReadLine(); //Input
+//Console.WriteLine("Hello" + ", " + UserName + "!"); //Output
+//string interpolation
+Console.WriteLine($"Hello, {UserName} !"); //Output
